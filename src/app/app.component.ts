79d8889aafbb6a1f145061323aac5abb26794bcb
xianda31 <99276316+xianda31@users.noreply.github.com>
2025-05-17 16:13:47 +0200

    V1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
